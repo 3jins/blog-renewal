4551c7c72ba84fdcbf46050f723f8b7d1f4846a4
Sejin Jeon <jinaidy93@gmail.com>
2020-12-20 11:28:28 +0900

    [#24] Replace console loggings with `LoggingUtil`

--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import 'reflect-metadata';
 import Koa from 'koa';
 import Router from '@koa/router';
@@ -7,17 +6,19 @@ import { Connection } from 'mongoose';
 import { Server } from 'http';
 import * as DbConnection from '@src/common/mongodb/DbConnectionUtil';
 import { handleError } from '@src/common/error/BlogErrorHandlingUtil';
+import { leaveLog } from '@src/common/logging/LoggingUtil';
+import LogLevel from '@src/common/logging/LogLevel';
 
 const connectToDb = () => {
   DbConnection.setConnection();
   const conn: Connection = DbConnection.getConnection();
-  conn.on('error', (err: Error) => console.error(err));
+  conn.on('error', (err: Error) => leaveLog(err.toString(), LogLevel.ERROR));
   conn.once('open', () => {
     const { name, host, port } = conn;
-    console.info('Connected to MongoDB server.');
-    console.info(` - name: ${name}`);
-    console.info(` - host: ${host}`);
-    console.info(` - port: ${port}`);
+    leaveLog('Connected to MongoDB server.', LogLevel.INFO);
+    leaveLog(` - name: ${name}`, LogLevel.INFO);
+    leaveLog(` - host: ${host}`, LogLevel.INFO);
+    leaveLog(` - port: ${port}`, LogLevel.INFO);
   });
 };
 
@@ -33,7 +34,7 @@ const makeApp = (router: Router): Koa => {
     .use(async (ctx, next) => {
       const { ip } = ctx.request;
       const rt = ctx.response.get('X-Response-Time');
-      console.info(`${ctx.method} ${ctx.url} - ${rt} from ${ip}`);
+      leaveLog(`${ctx.method} ${ctx.url} - ${rt} from ${ip}`, LogLevel.INFO);
       await next();
     })
     .use((ctx, next) => next()
@@ -47,7 +48,7 @@ const startApp = (routerList: Router[]): Server => {
   const { port } = config.get('server');
   const router = makeRouter(routerList);
   const app = makeApp(router);
-  return app.listen(port, () => console.info(`Server started to listening from port ${port}.`));
+  return app.listen(port, () => leaveLog(`Server started to listening from port ${port}.`, LogLevel.INFO));
 };
 
 const endApp = (server: Server) => server.close();
