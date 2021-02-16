59131183808e2a96ebf95624d9a0b2e52515ec86
Sejin Jeon <jeon.sejin@navercorp.com>
2021-02-17 01:16:27 +0900

    [#9] Modify createPost API to receive a markdown file

--- backend/test/post/postServiceTest.ts
+++ backend/test/post/postServiceTest.ts
@@ -3,22 +3,41 @@ import { deepEqual, instance, mock, verify } from 'ts-mockito';
 import PostService from '@src/post/PostService';
 import PostRepository from '@src/post/PostRepository';
 import { AddPostParamDto, CreatePostRepoParamDto } from '@src/post/PostDto';
-import { common as commonTestData } from '@test/data/testData';
+import { appPath, common as commonTestData } from '@test/data/testData';
+import fs from 'fs';
+import { File } from 'formidable';
+import Language from '@src/common/constant/Language';
 
 describe('PostService test', () => {
   let postService: PostService;
   let postRepository: PostRepository;
   let serviceParamDto: AddPostParamDto;
+  let post: File;
+  let fileContent: string;
 
   before(() => {
-    serviceParamDto = {
-      title: commonTestData.post1.title,
-      rawContent: commonTestData.post1.rawContent,
-      language: commonTestData.post1.language,
-      thumbnailContent: commonTestData.post1.thumbnailContent,
-      createdDate: new Date(),
+    const fileName = 'test.md';
+    const filePath = `${appPath.testData}/${fileName}`;
+    const fileStream: Buffer = fs.readFileSync(filePath);
+    fileContent = fileStream.toString();
+    const fileStat: fs.Stats = fs.statSync(filePath);
+    post = {
+      size: fileStream.byteLength,
+      path: filePath,
+      name: fileName,
+      type: 'application/octet-stream',
+      lastModifiedDate: fileStat.mtime,
+      toJSON(): Object {
+        return {};
+      },
     };
 
+    serviceParamDto = {
+      post,
+      seriesId: '1234',
+      language: Language.KO,
+      thumbnailContent: '뚜샤!',
+    };
     postRepository = mock(PostRepository);
     postService = new PostService(instance(postRepository));
     should();
@@ -28,7 +47,9 @@ describe('PostService test', () => {
     postService.createPost(serviceParamDto);
     const repoParamDto: CreatePostRepoParamDto = {
       ...serviceParamDto,
-      renderedContent: serviceParamDto.rawContent,
+      title: post.name,
+      rawContent: fileContent,
+      renderedContent: fileContent,
     };
     verify(postRepository.createPost(deepEqual(repoParamDto))).once();
   });
