1bb859e81e07d8b806ee5e937fc9f06e006917d0
Sejin Jeon <jinaidy93@gmail.com>
2021-02-28 17:18:21 +0900

    [#0] Fix to leave the complete error info as log when an unexpected error is occured

--- backend/src/common/error/BlogErrorHandlingUtil.ts
+++ backend/src/common/error/BlogErrorHandlingUtil.ts
@@ -4,7 +4,9 @@ import BlogError from '@src/common/error/BlogError';
 import { BlogErrorCode } from '@src/common/error/BlogErrorCode';
 
 export const handleError = (ctx: Context, error: Error) => {
-  const blogError: BlogError = error instanceof BlogError ? error as BlogError : new BlogError(BlogErrorCode.UNEXPECTED_ERROR, [], error.message);
+  const blogError: BlogError = error instanceof BlogError
+    ? error as BlogError
+    : new BlogError(BlogErrorCode.UNEXPECTED_ERROR, [], error.toString());
   const { blogErrorCode: { httpErrorCode, errorMessage } } = blogError;
   ctx.status = httpErrorCode;
   ctx.body = { message: errorMessage };
