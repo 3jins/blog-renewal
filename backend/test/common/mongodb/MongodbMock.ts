8ef36a9164b00fdbc8fe7286696e6a309c799724
Sejin Jeon <jinaidy93@gmail.com>
2021-06-20 17:21:23 +0900

    [#67] Add tests for `useTransaction`

---
+++ backend/test/common/mongodb/MongodbMock.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-empty-function */
+import { ClientSession, Connection, Promise } from 'mongoose';
+
+// @ts-ignore
+export class ClientSessionForTest implements ClientSession {
+  /*
+   * Since the original one(ClientSession) is an interface, not a specific class, mock implementation is needed to be stubbed.
+   * If the implementation mock(ClientSessionForTest) should be modified for the change of the test scenario, refer `mongoose.ClientSession`.
+   */
+
+  abortTransaction = (): Promise<void> => Promise.resolve(undefined);
+
+  commitTransaction = (): Promise<void> => Promise.resolve(undefined);
+
+  endSession = (): void => {};
+
+  startTransaction = (): void => {};
+}
+
+export class ConnectionForTest extends Connection {
+  clientSessionStub: ClientSessionForTest;
+
+  constructor(clientSessionStub: ClientSessionForTest) {
+    // @ts-ignore
+    super(undefined);
+    this.clientSessionStub = clientSessionStub;
+  }
+
+  startSession = (): Promise<ClientSession> => new Promise((resolve) => resolve(this.clientSessionStub));
+}
