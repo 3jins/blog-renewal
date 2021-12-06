c8404c9137b107db4cc65d279e886a80bbe716fb
Sejin Jeon <jinaidy93@gmail.com>
2021-12-06 09:16:50 +0900

    [#11] Add `findPost` to `PostRouter`

---
+++ backend/src/post/dto/PostDtoMapper.ts
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+import { FindPostRequestDto } from '@src/post/dto/PostRequestDto';
+import { FindPostParamDto } from '@src/post/dto/PostParamDto';
+import Language from '@src/common/constant/Language';
+
+export const mapFindPostRequestDtoToFindPostParamDto = (source: FindPostRequestDto): FindPostParamDto => {
+  const destination: FindPostParamDto = {};
+  Object.keys(source)
+    .forEach((key) => {
+      const assignObject = {};
+      if (_.isNil(source[key])) {
+        return;
+      }
+      if (key === 'language') {
+        assignObject[key] = source[key] as Language;
+        Object.assign(destination, assignObject);
+      }
+      if (['updateDateFrom', 'updateDateTo'].includes(key)) {
+        assignObject[key] = new Date(source[key]);
+        Object.assign(destination, assignObject);
+      }
+      assignObject[key] = source[key];
+      Object.assign(destination, assignObject);
+    });
+  return destination;
+};
--- backend/src/post/dto/PostRequestDto.ts
+++ backend/src/post/dto/PostRequestDto.ts
@@ -1,6 +1,26 @@
 import { JSONSchemaType } from 'ajv';
 import Language from '@src/common/constant/Language';
 
+export interface FindPostRequestDto {
+  postNo?: number;
+  categoryId?: string;
+  seriesId?: string;
+  tagIdList?: string[];
+  isPrivate?: boolean;
+  isDeprecated?: boolean;
+  isDraft?: boolean;
+  title?: string;
+  rawContent?: string;
+  renderedContent?: string;
+  language?: string;
+  thumbnailContent?: string;
+  thumbnailImageId?: string;
+  updateDateFrom?: string;
+  updateDateTo?: string;
+  isLatestVersion?: boolean;
+  isOnlyExactSameFieldFound?: boolean;
+}
+
 export interface CreateNewPostRequestDto {
   // post meta
   categoryName?: string;
@@ -31,6 +51,30 @@ export interface UpdatePostMetaDataRequestDto {
   isDeprecated?: boolean;
   isDraft?: boolean;
 }
+
+export const FindPostRequestSchema: JSONSchemaType<FindPostRequestDto> = {
+  type: 'object',
+  additionalProperties: false,
+  properties: {
+    postNo: { type: 'number', nullable: true },
+    categoryId: { type: 'string', nullable: true },
+    seriesId: { type: 'string', nullable: true },
+    tagIdList: { type: 'array', nullable: true, items: { type: 'string' } },
+    isPrivate: { type: 'boolean', nullable: true },
+    isDeprecated: { type: 'boolean', nullable: true },
+    isDraft: { type: 'boolean', nullable: true },
+    title: { type: 'string', nullable: true },
+    rawContent: { type: 'string', nullable: true },
+    renderedContent: { type: 'string', nullable: true },
+    language: { type: 'string', enum: Object.values(Language), nullable: true },
+    thumbnailContent: { type: 'string', nullable: true },
+    thumbnailImageId: { type: 'string', nullable: true },
+    updateDateFrom: { type: 'string', format: 'date-time', nullable: true },
+    updateDateTo: { type: 'string', format: 'date-time', nullable: true },
+    isLatestVersion: { type: 'boolean', nullable: true },
+    isOnlyExactSameFieldFound: { type: 'boolean', nullable: true },
+  },
+};
 
 export const CreateNewPostRequestSchema: JSONSchemaType<CreateNewPostRequestDto> = {
   type: 'object',
